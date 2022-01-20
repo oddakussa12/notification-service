9ee1d9e44c4a43f31f5aab86f72cc05809043bef
Odda Kussa <oddakussa@outlook.com>
2022-01-20 02:19:28 -0500

    autoreply feature

---
+++ app/Http/Controllers/AutoReplyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AutoReply;
+use Illuminate\Http\Request;
+
+class AutoReplyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AutoReply  $autoReply
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AutoReply $autoReply)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AutoReply  $autoReply
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AutoReply $autoReply)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AutoReply  $autoReply
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AutoReply $autoReply)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AutoReply  $autoReply
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AutoReply $autoReply)
+    {
+        //
+    }
+}
